ff1d5842180babadb7561077d6327fea08df541f
Damián Silvani <munshkr@gmail.com>
2012-06-15 20:08:35 -0300

    initial gem commit

---
+++ lib/freeling-analyzer.rb
@@ -0,0 +1,7 @@
+require "freeling-analyzer/version"
+
+module Freeling
+  module Analyzer
+    # Your code goes here...
+  end
+end
---
+++ lib/freeling-analyzer/version.rb
@@ -0,0 +1,5 @@
+module Freeling
+  module Analyzer
+    VERSION = "0.0.1"
+  end
+end
