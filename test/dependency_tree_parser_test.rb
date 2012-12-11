942101a53345613fd182005b8d825b198f1f764a
Damián Silvani <munshkr@gmail.com>
2012-12-10 21:41:25 -0300

    dep: Add more tests, with a failing one

--- test/dependency_tree_parser_test.rb
+++ test/dependency_tree_parser_test.rb
@@ -6,6 +6,11 @@ class DependencyTreeParserTest < MiniTest::Unit::TestCase
     @parser = FreeLing::Analyzer::DependencyTreeParser.new
   end
 
+  def test_empty_tree
+    res = parse("")
+    assert_equal("", res)
+  end
+
   def test_single_node
     res = parse("s-a-fs/adj-mod/(argentina argentino AQ0FS0 -)")
     assert_equal({
@@ -49,9 +54,12 @@ class DependencyTreeParserTest < MiniTest::Unit::TestCase
   end
 
   def test_tree_with_single_node
-    res = parse("sn/obj-prep/(nacionalidad nacionalidad NCFS000 -) [\n" \
-                  "s-a-fs/adj-mod/(argentina argentino AQ0FS0 -)\n" \
-                "]")
+    tree = <<-TREE
+      sn/obj-prep/(nacionalidad nacionalidad NCFS000 -) [
+        s-a-fs/adj-mod/(argentina argentino AQ0FS0 -)
+      ]
+    TREE
+    res = parse(tree)
     assert_equal({
       ancestor_label: "sn",
       dependence_label: "obj-prep",
@@ -75,10 +83,13 @@ class DependencyTreeParserTest < MiniTest::Unit::TestCase
   end
 
   def test_tree_with_multiple_nodes
-    res = parse("s-adj/adj-mod/(, , Fc -) [\n" \
-                  "s-a-ms/modnomatch/(casado casar VMP00SM -)\n" \
-                  "s-a-ms/modnomatch/(nacido nacer VMP00SM -)\n" \
-                "]")
+    tree = <<-TREE
+      s-adj/adj-mod/(, , Fc -) [
+        s-a-ms/modnomatch/(casado casar VMP00SM -)
+        s-a-ms/modnomatch/(nacido nacer VMP00SM -)
+      ]
+    TREE
+    res = parse(tree)
     assert_equal({
       ancestor_label: "s-adj",
       dependence_label: "adj-mod",
@@ -111,9 +122,97 @@ class DependencyTreeParserTest < MiniTest::Unit::TestCase
     }, res)
   end
 
-  def test_empty_tree
-    res = parse("")
-    assert_equal("", res)
+  def test_tree_within_a_tree
+    tree = <<-TREE
+      grup-verb/top/(es ser VSIP3S0 -) [
+        interjeccio/ador/(Hola hola I -) [
+          Fc/modnomatch/(, , Fc -)
+        ]
+      ]
+    TREE
+    res = parse(tree)
+    assert_equal({
+      ancestor_label: "grup-verb",
+      dependence_label: "top",
+      token: {
+        form: "es",
+        lemma: "ser",
+        tag: "VSIP3S0",
+        coref_group: "-"
+      },
+      children: {
+        ancestor_label: "interjeccio",
+        dependence_label: "ador",
+        token: {
+          form: "Hola",
+          lemma: "hola",
+          tag: "I",
+          coref_group: "-"
+        },
+        children: {
+          ancestor_label: "Fc",
+          dependence_label: "modnomatch",
+          token: {
+            form: ",",
+            lemma: ",",
+            tag: "Fc",
+            coref_group: "-"
+          }
+        }
+      }
+    }, res)
+  end
+
+  def test_tree_with_a_tree_and_a_node
+    tree = <<-TREE
+      grup-verb/top/(es ser VSIP3S0 -) [
+        interjeccio/ador/(Hola hola I -) [
+          Fc/modnomatch/(, , Fc -)
+        ]
+        sn/att/(Juan juan NP00SP0 -)
+      ]
+    TREE
+    res = parse(tree)
+    assert_equal({
+      ancestor_label: "grup-verb",
+      dependence_label: "top",
+      token: {
+        form: "es",
+        lemma: "ser",
+        tag: "VSIP3S0",
+        coref_group: "-"
+      },
+      children: [{
+          ancestor_label: "interjeccio",
+          dependence_label: "ador",
+          token: {
+            form: "Hola",
+            lemma: "hola",
+            tag: "I",
+            coref_group: "-"
+          },
+          children: {
+            ancestor_label: "Fc",
+            dependence_label: "modnomatch",
+            token: {
+              form: ",",
+              lemma: ",",
+              tag: "Fc",
+              coref_group: "-"
+            }
+          }
+        }, {
+          ancestor_label: "sn",
+          dependence_label: "att",
+          token: {
+            form: "Juan",
+            lemma: "juan",
+            tag: "NP00SP0",
+            coref_group: "-"
+          },
+        }
+      ]
+    }, res)
   end
 
 private
@@ -125,8 +224,10 @@ private
   end
 
   def print_text(string)
+    puts
     string.split("\n").each.with_index do |line, index|
       puts "#{(index + 1).to_s.rjust(4)}_#{line}"
     end
+    puts
   end
 end
