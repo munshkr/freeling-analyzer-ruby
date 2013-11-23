16022c3ff35efae3b4f4ad0076e475fc34be0d44
malev <marcosvanetta@gmail.com>
2013-11-23 17:28:46 -0300

    Unskip test
    I removed the probability tests since they are tied to a single
    freeling version. Tests should be freeling versions independant.

--- test/analyzer_test.rb
+++ test/analyzer_test.rb
@@ -9,8 +9,6 @@ class AnalyzerTest < MiniTest::Unit::TestCase
   end
 
   def test_token_attributes
-    skip
-
     expected_token = { form: "El", lemma: "el", prob: 1, tag: "DA0MS0" }
 
     analyzer = FreeLing::Analyzer.new(@a, :language => :es)
@@ -22,22 +20,20 @@ class AnalyzerTest < MiniTest::Unit::TestCase
   end
 
   def test_token_list
-    skip
-
     expected_tokens = [
-      { form: "El", lemma: "el", prob: 1, tag: "DA0MS0" },
-      { form: "gato", lemma: "gato", prob: 1, tag: "NCMS000" },
-      { form: "come", lemma: "comer", prob: 0.75, tag: "VMIP3S0" },
-      { form: "pescado", lemma: "pescado", prob: 0.833333, tag: "NCMS000" },
-      { form: "y", lemma: "y", prob: 0.999812, tag: "CC" },
-      { form: "bebe", lemma: "beber", prob: 0.994868, tag: "VMIP3S0" },
-      { form: "agua", lemma: "agua", prob: 0.973333, tag: "NCCS000" },
-      { form: ".", lemma: ".", prob: 1, tag: "Fp" },
+      { form: "El",      lemma: "el",      tag: "DA0MS0"  },
+      { form: "gato",    lemma: "gato",    tag: "NCMS000" },
+      { form: "come",    lemma: "comer",   tag: "VMIP3S0" },
+      { form: "pescado", lemma: "pescado", tag: "NCMS000" },
+      { form: "y",       lemma: "y",       tag: "CC"      },
+      { form: "bebe",    lemma: "beber",   tag: "VMIP3S0" },
+      { form: "agua",    lemma: "agua",    tag: "NCCS000" },
+      { form: ".",       lemma: ".",       tag: "Fp"      },
     ]
 
     analyzer = FreeLing::Analyzer.new(@a, :language => :es)
     analyzer.tokens.each.with_index do |token, i|
-      [:form, :lemma, :prob, :tag].each do |key|
+      [:form, :lemma, :tag].each do |key|
         assert_equal expected_tokens[i][key], token[key]
       end
     end
